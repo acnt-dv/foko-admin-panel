aff253a68a9bc2b5a3af6e2f31c7110c748547b3
dv <acnt.dv@gmail.com>
2025-09-24 16:57:22 +0330

    reset gallery data on new project adding

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -171,6 +171,7 @@ export default function Projects() {
                             // category: ''
                         });
                         setProjectData([{ key: '', value: '' }]);
+                        setGalleryFormData(null);
                         setModalOpen(true);
                     }}
                     className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700"
